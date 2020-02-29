b491ce584142160f1cb5f98770f5088e029fa7f4
rogersoh <jaggedfel2560@gmail.com>
2020-03-01 00:12:35 +0800

    add code

---
+++ soil_moisture_meter/soil_moisture_meter.ino
@@ -0,0 +1,41 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define MOISTUREREADPIN 2
+
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     9 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  Serial.begin(9600); // open serial port, set the baud rate as 9600 bps
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Don't proceed, loop forever
+  }
+}
+
+void loop() {
+  int moisturereading;
+  moisturereading = analogRead(MOISTUREREADPIN); //connect sensor to Analog 0
+  Serial.println(moisturereading); //print the value to serial port
+
+  display.clearDisplay();
+
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0, 0);            // Start at top-left corner
+  display.println(F("Soil Moisture"));
+  display.println(F("(550 dry, 295 wet)"));
+  display.setTextSize(2);
+  display.println(moisturereading);
+  display.display();
+  delay(2000);
+
+}
